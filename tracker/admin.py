2876aa4849b45e2b782c188ced090ed2af136f61
Grace Barrameda <ga.barrameda@gmail.com>
2015-03-27 16:40:37 +0800

    name clothing plains

--- tracker/admin.py
+++ tracker/admin.py
@@ -1,3 +1,5 @@
 from django.contrib import admin
+from .models import Clothingplain
 
 # Register your models here.
+admin.site.register(Clothingplain)
--- tracker/models.py
+++ tracker/models.py
@@ -4,7 +4,7 @@ from django.db import models
 class Clothingplain(models.Model):
 	name = models.CharField(max_length=200)
 
-	def __unicode__(self):
+	def __str__(self):
 		return self.name
 
 # class Clothingeotagged(models.Model):
