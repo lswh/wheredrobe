bf64173a529f28ad67a0306ddde8e9a63a287b1e
Grace Barrameda <ga.barrameda@gmail.com>
2015-03-27 16:31:14 +0800

    added basic clothing model

--- tracker/models.py
+++ tracker/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
 # Create your models here.
+class Clothingplain(models.Model):
+	name = models.CharField(max_length=200)
+
+	def __unicode__(self):
+		return self.name
+
+# class Clothingeotagged(models.Model):
+
+# class LaundryShop
+
+# class AlterShop 
