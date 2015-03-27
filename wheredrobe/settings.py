0a4e531f537253fed1c627398986942dd2e6647f
Grace Barrameda <ga.barrameda@gmail.com>
2015-03-27 16:05:32 +0800

    included apps in settings

--- wheredrobe/settings.py
+++ wheredrobe/settings.py
@@ -36,6 +36,10 @@ INSTALLED_APPS = (
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'laundrolocator',
+    'tracker',
+    'wheredrobe'
+    'journal',
 )
 
 MIDDLEWARE_CLASSES = (
