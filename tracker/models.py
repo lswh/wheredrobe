2978dd25b6bcbdca1e18eea0167684b5d2f28d3f
heleyna <labao.hm@gmail.com>
2015-04-01 10:00:48 +0800

    added status choice field

--- tracker/models.py
+++ tracker/models.py
@@ -3,6 +3,22 @@ from django.db import models
 # Create your models here.
 class Clothingplain(models.Model):
 	name = models.CharField(max_length=200)
+	#Choice Field for Status
+	CABINET = 'CB'
+	REPAIRSHOP = 'RS'
+	HAMPER = 'H'
+	LAUNDRYSHOP = 'LS'
+	MISSING = 'M'
+	status_choices = (
+		(CABINET,'In the Cabinet'),
+		(REPAIRSHOP,'Under Alterations'),
+		(HAMPER,'In the Hamper'),
+		(LAUNDRYSHOP,'At the Laundry Shop'),
+		(MISSING,'Missing'),
+		)
+	itemstatus = models.CharField(max_length=2, choices=status_choices, default=CABINET)
+	#picture = models.ImageField()
+
 
 	def __str__(self):
 		return self.name
