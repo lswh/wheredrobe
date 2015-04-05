1485ad1f8b9035a5e215bd2634bf72db198748e4
Helen Mary Labao <labao.hm@gmail.com>
2015-04-05 17:52:53 +0800

    view html linked to model

--- tracker/models.py
+++ tracker/models.py
@@ -1,4 +1,7 @@
 from django.db import models
+from django.core.files.storage import FileSystemStorage
+
+#fs = FileSystemStorage(location='/mediatemp')
 
 # Create your models here.
 class Clothingplain(models.Model):
