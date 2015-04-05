a6fd7feb66ee90f3845b61c5cd0f1055045be77e
Helen Mary Labao <labao.hm@gmail.com>
2015-04-05 17:17:43 +0800

    added the views for the tracker

--- tracker/views.py
+++ tracker/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
+from django.utils import timezone
+from .models import Clothingplain
 
 # Create your views here.
+def completeclothinglist(request):
+    posts = Clothingplain.objects.order_by('name')
+    return render(request, 'tracker/completeclothinglist.html', {'posts': posts})
